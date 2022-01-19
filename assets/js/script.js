0f75399fad69100fa7f80a3c77134fa46dfcf1af
Gerson Apablaza <gerson.eaa@gmail.com>
2022-01-18 21:25:50 -0300

    Estructura inicial reutilizada

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+// función Bootstrap para aplicar los tooltips definidos en toda la página
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(function() {
+    
+    // simula el envío de un email al presionar el botón
+    $( "#enviarCorreo" ).click(function() {
+        alert("El correo fue enviado correctamente");
+    });
+
+    // cambia el color de las letras de una sección específica al hacer doble-click
+    $( ".content h3").on( "dblclick", function() {
+        var elem = $(this);
+        elem.css("color", "red");
+    });
+
+    // hace aparecer/desaparecer el contenido de una tarjeta
+    $( ".card-title ").on( "click", function() {
+        var card = $(this);
+        card.next(".card-text").toggle();
+    });
+
+});
